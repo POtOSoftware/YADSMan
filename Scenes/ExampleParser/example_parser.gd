2372a9ec9293b9d812d5b6b52eb224fe5a3b878d
Kyle Kailihiwa <kylekailihiwa@gmail.com>
2025-03-16 15:23:47 +0800

    variables are parse and added into the text like a good boy :3

--- Scenes/ExampleParser/example_parser.gd
+++ Scenes/ExampleParser/example_parser.gd
@@ -5,8 +5,19 @@ extends Control
 var speaker_lines: Array[String]
 var dialogue_lines: Array[String]
 var specfunc_lines: Array[String]
+var variable_names: Array
+var variable_values: Array
 
 func _on_load_button_pressed() -> void:
 	speaker_lines = FileManager.load_speaker_lines_from_file(file_load_string.text)
 	dialogue_lines = FileManager.load_dialogue_lines_from_file(file_load_string.text)
 	specfunc_lines = FileManager.load_specfuncs_from_file(file_load_string.text)
+	
+	variable_names = FileManager.load_var_names_from_file(file_load_string.text)
+	variable_values = FileManager.load_var_values_from_file(file_load_string.text)
+
+func test_func() -> void:
+	print("the test function has been called and executed :3")
+
+func func_exit() -> void:
+	get_tree().quit()
--- Scenes/ExampleParser/yads_parser.gd
+++ Scenes/ExampleParser/yads_parser.gd
@@ -9,17 +9,26 @@ extends Control
 var current_dialogue_index: int = 0
 
 func _on_next_button_pressed() -> void:
+	parse_variables_in_line(current_dialogue_index)
+	
 	speaker_label.text = root.speaker_lines[current_dialogue_index]
 	dialogue_label.text = root.dialogue_lines[current_dialogue_index]
 	
 	if root.specfunc_lines[current_dialogue_index] != "":
 		print("CALLING SPECIAL FUNCTION: " + root.specfunc_lines[current_dialogue_index])
-		call(root.specfunc_lines[current_dialogue_index])
+		root.call(root.specfunc_lines[current_dialogue_index])
 	
 	current_dialogue_index += 1
 
-func test_func() -> void:
-	print("the test function has been called and executed :3")
-
-func func_exit() -> void:
-	get_tree().quit()
+func parse_variables_in_line(_current_line_index: int) -> void:
+	var _current_var_names: Array = root.variable_names
+	var _current_var_values: Array = root.variable_values
+	
+	#print(root.dialogue_lines[_current_line_index])
+	
+	for _var in range(_current_var_names.size()):
+		print(_current_var_names[_var])
+		# this is some horse shitty long line but it works
+		# and its gonna get reworked anyways, cause we going
+		# dicktionaries instead of stinky stinky fuck ass arrays
+		root.dialogue_lines[_current_line_index] = root.dialogue_lines[_current_line_index].replace("$" + _current_var_names[_var] + "$", _current_var_values[_var])
