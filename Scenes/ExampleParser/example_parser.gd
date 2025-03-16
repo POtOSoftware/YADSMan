1221a30d7b6f9aa87272f4de9645445a240bbc16
Kyle Kailihiwa <kylekailihiwa@gmail.com>
2025-03-16 16:18:10 +0800

    Revert "loads yads file into dictionary like a good boy :3"
    This reverts commit 7650ef963c7ef89666107d70569304d1e8d12eff.

--- Scenes/ExampleParser/example_parser.gd
+++ Scenes/ExampleParser/example_parser.gd
@@ -9,8 +9,6 @@ var variable_names: Array
 var variable_values: Array
 
 func _on_load_button_pressed() -> void:
-	YadsManager.load_yads_file_to_dictionary()
-	
 	speaker_lines = FileManager.load_speaker_lines_from_file(file_load_string.text)
 	dialogue_lines = FileManager.load_dialogue_lines_from_file(file_load_string.text)
 	specfunc_lines = FileManager.load_specfuncs_from_file(file_load_string.text)
